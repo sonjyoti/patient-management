35f9e5057efe83e6619829a43161e606b9abc389
Sonjyoti Rabha <sonjyoti33@gmail.com>
2026-01-20 19:21:43 +0530

    Initial commit

---
+++ patient-service/src/test/com/pm/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package test.com.pm.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
